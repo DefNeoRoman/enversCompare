9eb0292fce182fc5dba6b304f64edb3b7227c8bc
DefNeoRoman <d89056411060@yandex.ru>
2020-03-22 10:53:49 +0300

    del

--- src/main/kotlin/app/feature/PersonRepository.kt
+++
@@ -1,8 +0,0 @@
-package app.feature
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-
-@Repository
-interface PersonRepository : CrudRepository<Person, Int>
